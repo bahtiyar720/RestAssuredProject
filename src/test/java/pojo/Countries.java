b1f0d4fe2a3b2e8ff0e1bdeb4412a755cd886ac3
bahtiyar720 <bahtiyar720@gmail.com>
2020-09-03 11:17:44 -0700

    day 10  commit after lunch

--- src/test/java/pojo/Countries.java
+++ src/test/java/pojo/Countries.java
@@ -1,4 +1,15 @@
 package pojo;
+
 //TODO Country POJO class that have same encapsulated field as Countries table
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Countries {
+    private String country_id;
+    private String country_name;
+    private int region_id;
 }
